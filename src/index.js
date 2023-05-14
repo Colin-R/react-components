346a24f569c3e96c1c24c4f7d92a22d258bc7f2e
Colin Ross <cross@twig-world.com>
2023-05-14 21:01:54 +0100

    Exporting pages and components via index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+import ErrorPage from "./pages/ErrorPage";
+import LinkBackPage from "./pages/LinkBackPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RootPage from "./pages/RootPage";
+
+import ChoiceBox from "./components/ChoiceBox";
+import FormBox from "./components/FormBox";
+import InputField from "./components/InputField";
+import LinkButton from "./components/LinkButton";
+import SiteHeader from "./components/SiteHeader";
+import SubmitButton from "./components/SubmitButton";
+import SubTitle from "./components/SubTitle";
+import Title from "./components/Title";
+
 function colinRReactComponents() {
     return "hello colin-r-react-components";
 }
 
-module.exports = colinRReactComponents;
+export {
+    ErrorPage,
+    LinkBackPage,
+    NotFoundPage,
+    RootPage,
+    ChoiceBox,
+    FormBox,
+    InputField,
+    LinkButton,
+    SiteHeader,
+    SubmitButton,
+    SubTitle,
+    Title,
+    colinRReactComponents,
+};
